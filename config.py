1643b8a4efc40ae6db902cac34a4dc5aee44515f
sohamsingh419 <skhackeriam@gmail.com>
2026-08-30 12:48:58 +0530

    Update configuration values in config.py

--- config.py
+++ config.py
@@ -3,34 +3,34 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "35822069"))
+API_ID = int(os.environ.get("API_ID", "32772113"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","35cea1c5e6384f57e914ac982ff5ffd1")
+API_HASH = os.environ.get("API_HASH","79b81186aefef9ca9216c646152ffe45")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8309873942:AAHKpbr7GhXNjOiupMfpdBeNSsF2jBdH1dg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8672812620:AAGfAJ6E6Lt10cllI271k1TYPztImpAyrnU")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@extract_txt_new_bot")
-BOT_TEXT = "℘ཞıŋƈɛ℘ıą"
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@GolbalXtxtExtractor_bot")
+BOT_TEXT = "GolbalXtxtExtractor bot"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "5938871512"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7028236763"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003766078927"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1004496808235"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003766078927")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003907469981")) 
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://princepiax:princepia45@cluster0.4kn7hq0.mongodb.net/?appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://simrankaurrani262_db_user:X5gh7XE07vAFkeoC@cluster0.ysyu13h.mongodb.net/?appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003766078927"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003920249676"))
 # -----------------------------------------------
-join = '<a href="https://t.me/princepia">✳️ JOIN BACKUP</a>'
+join = '<a href="https://t.me/Globall_X">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Princepia" #without @
+ADMIN_BOT_USERNAME = "Global_X_SohaN" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://imgcdn.stablediffusionweb.com/2024/4/17/53a08e9d-950a-4c1b-8808-767ea2cea7e6.jpg")
 
